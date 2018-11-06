65b9370861534776ab3311b96bb6c3e3454df0ce
Jacob Winkler <jwinkler1313@gmail.com>
2018-11-06 14:21:16 -0500

    Web page functioning
    Web page is done, needs testing and cleanup

--- truth.rb
+++ truth.rb
@@ -3,39 +3,56 @@ require 'sinatra/reloader'
 
 # The secret number to guess.  This will start as an invalid value
 # Generate a secret number from 1 to 100
-def secret_num(test)
-  if test.nil?
-    rand(1..100)
-  else
-    test = test.to_i
-    if (test > 100) || (test < 1) ||(!test.is_a? Integer)
-	  50
-	else
-      test
-	end
+
+
+def get_binary(numbits)
+  arr = []
+  maxnum = 2**numbits
+  (0..maxnum-1).each do |i|
+    arr.push(("%0"+numbits.to_s+"b")%i)
   end
+  arr
 end
 
-# Determine the message to send depending on whether the guess is higher,
-# lower, or the same as the secret number
-def determine_msg(number, guess)
-  to_return = ""
-  if guess == number
-    to_return = "That's exactly right!"
-  elsif guess > number
-    to_return = "That's too high!"
-  else
-    to_return = "That's too low!"
+def get_ands(arr,truthSym,falseSym)
+  andres = []
+  for i in arr
+      result = /0/ =~ i
+	  if(result.nil?)
+	    andres.push(truthSym)
+	  else
+	    andres.push(falseSym)
+	  end
   end
-  if (number - guess).abs > 10
-    to_return += " You're way off!"
-  end
-  if ((number - guess).abs <= 5) &&  ((number - guess).abs != 0)
-    to_return += " You're really close!"
-  end
-  to_return
+  andres
 end
 
+def get_ors(arr,truthSym,falseSym)
+  orres = []
+  for i in arr
+      result = /1/ =~ i
+	  if(result.nil?)
+	    orres.push(falseSym)
+	  else
+	    orres.push(truthSym)
+	  end
+  end
+  orres
+end
+
+def get_xors(arr,truthSym,falseSym)
+  xorres = []
+  for i in arr
+    temp = i.gsub(/[0]/, '')
+	puts(temp)
+	  if((temp.length % 2) != 0)
+	    xorres.push(truthSym)
+	  else
+	    xorres.push(falseSym)
+	  end
+  end
+  xorres
+end
 # If a GET request comes in at /, do the following.
 
 get '/' do
@@ -49,12 +66,15 @@ get '/' do
 	tabsize = -1
   else
     tabsize = tabsize.to_i
-    tabsize *= tabsize	
   end
   
+  strings = get_binary(tabsize)
+  andres = get_ands(strings,truthSym,falseSym)
+  orres = get_ors(strings,truthSym,falseSym)
+  xorres = get_xors(strings,truthSym,falseSym)
   # If there was no guess parameter, this is a brand new game.
   # Generate a secret number and set the guess to nil.
-  erb :index, :locals => { truthSym:truthSym, falseSym:falseSym, tabsize:tabsize }
+  erb :index, :locals => { truthSym:truthSym, falseSym:falseSym, tabsize:tabsize, strings:strings, andres:andres, orres:orres, xorres:xorres }
 end
 
 not_found do
